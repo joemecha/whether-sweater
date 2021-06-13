411d3118c4c5b51bebf82f7f5b1c4f03f98a6c7b
joemecha <jwmecha@gmail.com>
2021-06-12 23:04:41 -0500

    Add comment for next step of error handling

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    if params(:location).nil? # || add regex to check <word>,2-letters
+    if params(:location).nil? # || add regex to check <word>,2-letters || add if extra params
       render json: { errors: 'Incorrect query params' }, status: :bad_request # check project specs for error reqs if any
     else
       forecast = WeatherFacade.get_forecast_for_a_city(params[:location])
