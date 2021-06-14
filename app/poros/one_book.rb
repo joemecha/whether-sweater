107deb56d6257711dfb06530f9015a6916115905
joemecha <jwmecha@gmail.com>
2021-06-14 12:04:48 -0500

    Complete One Book poro and test

---
+++ app/poros/one_book.rb
@@ -0,0 +1,11 @@
+class OneBook
+  attr_reader :isbn,
+              :title,
+              :publisher
+
+  def initialize(data)
+    @isbn = data[:isbn]
+    @title = data[:title]
+    @publisher = data[:publisher]
+  end
+end
