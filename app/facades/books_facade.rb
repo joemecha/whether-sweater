19404a9101b878066ff2685625be9f2a61866b58
joemecha <jwmecha@gmail.com>
2021-06-14 12:22:30 -0500

    Books Facade complete passes tests

--- app/facades/books_facade.rb
+++ app/facades/books_facade.rb
@@ -4,9 +4,8 @@ class BooksFacade
     lat = coord[:lat]
     lng = coord[:lng]
     weather_json = WeatherService.fetch_forecast_for_a_city(lat, lng)
-    BookForecast.new(weather_json[:current])
     
     book_json = BookService.fetch_books(location)
-    Book.new(book_json, weather_json)
+    Book.new(location, book_json, weather_json)
   end
 end
