97a95f239dbe0424ca90fc52ecfceab06a789983
joemecha <jwmecha@gmail.com>
2021-06-15 16:46:04 -0500

    Add sessions route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resource :users, only: [:create]
       get '/forecast', to: 'forecast#index'
       get '/backgrounds', to: 'backgrounds#index'
       get '/book-search', to: 'booksearch#index'
+      post '/users', to: 'users#create'
+      post '/sessions', to: 'sessions#login'
     end
   end
 end
